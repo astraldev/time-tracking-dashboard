a5586d79881b84c8083fe4c59eab02216a05e29e
Ekure Edem <61796351+astraldev@users.noreply.github.com>
2025-12-05 17:07:06 +0100

    update variable name

--- src/TimeTrackingDashboard.vue
+++ src/TimeTrackingDashboard.vue
@@ -3,7 +3,7 @@
     <UserReport class="col-span-1 row-span-1" @timeline-changed="timelineChanged" :timeline="current_timeline_index"/>
     <div class="grid md:grid-rows-3 lg:col-span-3 md:row-span-3 lg:row-span-1 lg:grid-rows-2 lg:grid-cols-3 gap-6 md:grid-cols-2 grid-rows-6 grid-cols-1">
       <Cards v-for="(data, index) in DashBoardData" :key="index" :card-data="data" :image="data.img"
-        :timeframe="current_timeline_index" />
+        :timeframe="timeline_index" />
     </div>
   </div>
 </template>
@@ -17,13 +17,13 @@ export default {
   components: { Cards, UserReport },
   data(){
     return {
-      current_timeline_index: 1,
+      timeline_index: 1,
       DashBoardData: parser(DATA)
     }
   },
   methods: {
     timelineChanged(timeline){
-      this.current_timeline_index = timeline
+      this.timeline_index = timeline
     }
   }
 }
